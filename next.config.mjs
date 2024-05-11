27e97c30358d11109b3680a1671af3befe555194
akhudiak <next.teacher@lectrum.io>
2024-05-11 10:25:55 +0100

    added env variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        NEXT_PUBLIC_SUPABASE_URL: "https://hfsatpvdhodhldwehfgo.supabase.co",
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhmc2F0cHZkaG9kaGxkd2VoZmdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU0MTkxMTQsImV4cCI6MjAzMDk5NTExNH0.z-yXqsgGnyaEVbY70L0QXp30advqIfeOyc0iFVmaFoM"
+    }
+};
 
 export default nextConfig;
